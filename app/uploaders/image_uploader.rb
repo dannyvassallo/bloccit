3619ab35dff5b4643c57ba02ab981a291e5a8b83
dannyvassallo <danielvassallo87@gmail.com>
2015-01-10 01:17:37 -0500

    added images to posts

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-class AvatarUploader < CarrierWave::Uploader::Base
+class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
@@ -26,23 +26,15 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
-  process :resize_to_fill => [200, 300]
+  process :resize_to_fill => [300, 300]
   #
   # def scale(width, height)
   #   # do something
   # end
 
   # Create different versions of your uploaded files:
-  version :tiny do
-    process :resize_to_fill => [20, 20]
-  end
-
-  version :small do
-    process :resize_to_fill => [30, 30]
-  end
-
-  version :profile do
-    process :resize_to_fill => [45, 45]
+  version :thumb do
+    process :resize_to_fill => [50, 50]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
