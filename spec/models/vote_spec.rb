a7e5deef83888f5a326fcc0fe5f409e5b5020d2c
dannyvassallo <danielvassallo87@gmail.com>
2015-01-27 18:45:12 -0500

    fixed vote spec

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,10 +1,18 @@
 require 'rails_helper'
 
 describe Vote do
+  
   descibe "validations" do
     describe "value validation" do
       it "only allows -1 or 1 as values" do
-        expect( @vote.value ).to eq( 1 || -1 )
+        up_vote = Vote.new(value: 1)
+        expect(v.valid?).to eq(true)
+
+        down_vote = Vote.new(value: -1)
+        expect(v2.valid?).to eq(true)
+
+        invalid_vote = Vote.new(value: 2)
+        expect(bad_v.valid?).to eq(false)
       end
     end
   end
